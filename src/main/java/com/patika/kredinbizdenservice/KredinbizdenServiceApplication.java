42384e9b29619b8c5a6cbf6cd1da76b8554b590e
Emirkan Karabulut <emirkankarbulutz@gmail.com>
2024-03-13 13:57:01 +0300

    v1

--- src/main/java/com/patika/kredinbizdenservice/KredinbizdenServiceApplication.java
+++ src/main/java/com/patika/kredinbizdenservice/KredinbizdenServiceApplication.java
@@ -1,13 +1,26 @@
 package com.patika.kredinbizdenservice;
 
+import com.patika.kredinbizdenservice.model.User;
+import com.patika.kredinbizdenservice.service.userService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class KredinbizdenServiceApplication {
-
+    private static final userService us = new userService();
     public static void main(String[] args) {
         SpringApplication.run(KredinbizdenServiceApplication.class, args);
+        User user1=new User("ali","kisa","abc@example.com", "abc123", "999999999", true);
+        User user2=new User("ahmet","uzun","abc@example.com", "xyz111", "111111111", true);
+        us.addUser(user1);
+        us.addUser(user2);
+
+        System.out.println(us.getUsers().toString());
     }
+
+
+
 
 }
